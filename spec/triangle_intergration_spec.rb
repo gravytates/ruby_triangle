c933420bd146f91c6f1c6770d8b90740df81efef
Grady Shelton and Kai de Rochemont <kai@>
2017-04-24 10:04:49 -0700

    pass integration test to show a result of scalene triangle

--- spec/triangle_intergration_spec.rb
+++ spec/triangle_intergration_spec.rb
@@ -23,4 +23,14 @@ describe('the triangle path', {:type => :feature}) do
     click_button('Go!')
     expect(page).to have_content('Isoceles!')
   end
+
+  it('processes user entry to determine type of triangle entered') do
+    visit('/')
+    fill_in('side1', :with => '5')
+    fill_in('side2', :with => '7')
+    fill_in('side3', :with => '6')
+
+    click_button('Go!')
+    expect(page).to have_content('Scalene!')
+  end
 end
